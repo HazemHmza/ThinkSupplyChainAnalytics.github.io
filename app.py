c71b23fe0753a1c54ce83aaae3e27fc75f6ca6d4
HazemHmza <105869950+HazemHmza@users.noreply.github.com>
2022-06-04 13:29:40 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -41,10 +41,10 @@ def generate_html_download_link(fig):
 
 st.set_page_config(page_title="Think Supply Chain Analysis Tool",page_icon=":roller_coaster:", layout='wide')
 st.title('Think Supply Chain Data Analysis Tool 📈')
-st.subheader('Designed By: Hazem Hamza ')
-st.write("[Let's Connect on LinkedIn](https://www.linkedin.com/in/hazem-hamza-mm-scm/)")
-# st.write("[Watch he video from here -->](https://youtu.be/criQYC4Zq70)")
-# st.write("[How to handle errors video from here -->](https://youtu.be/COgW2lKOjSU)")
+st.subheader('By: Hazem Hamza ')
+# st.write("[Let's Connect on LinkedIn](https://www.linkedin.com/in/hazem-hamza-mm-scm/)")
+# st.write("[Watch he video from here ](https://youtu.be/criQYC4Zq70)")
+# st.write("[How to handle errors video from here](https://youtu.be/COgW2lKOjSU)")
 st.subheader('Upload or Drag the Excel')
 
 # Use local CSS
